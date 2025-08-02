2f87747318c535997fffc9385a5546fa5920a5e5
Gaelle Tohme <122909212+Gaelle04@users.noreply.github.com>
2025-08-02 14:11:51 +0300

    created a list of students

--- BackendLab.Api/Program.cs
+++ BackendLab.Api/Program.cs
@@ -1,3 +1,6 @@
+using BackendLab.Api.Models;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -14,28 +17,13 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+var students = new List<Student>();
 
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast");
+students.Add(new Student(1, "gaelle", "gaelle@gmail.com"));
+students.Add(new Student(2, "chloe", "chloe@gmail.com"));
+students.Add(new Student(3, "chris", "chris@gmail.com"));
+students.Add(new Student(4, "John", "John@gmail.com"));
+
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
